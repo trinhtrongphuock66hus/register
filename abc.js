4892b811e6a7457e03993b4a92eb3202bd324d2a
trinhtrongphuock66hus <trinhtrongphuoc_t66@hus.edu.vn>
2023-04-30 22:25:24 +0700

    load

---
+++ abc.js
@@ -0,0 +1,12 @@
+let showPassword = document.getElementById('showPassword');
+let inputPassword = document.getElementById('password');
+showPassword.onclick = function(){
+    if(inputPassword.type == 'password'){
+        inputPassword.type = 'text';
+        showPassword.classList.add('show');
+    }
+    else{
+        inputPassword.type = 'password';
+        showPassword.classList.remove('show');
+    }
+}
